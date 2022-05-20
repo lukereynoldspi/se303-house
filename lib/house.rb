c00ae5543b3a1db87637a35d4f16c120018a7f16
Luke Reynolds <lukereynoldspi@gmail.com>
2022-05-20 13:56:10 -0700

    rooster function replaced strings

--- lib/house.rb
+++ lib/house.rb
@@ -20,11 +20,11 @@ class House
     when num == 9
       "#{intro} #{priest} #{man} #{maiden} #{cow} #{dog} #{cat} #{rat} #{malt} #{jack}.\n"
     when num == 10
-      "#{intro} rooster that crowed in the morn that woke the #{priest} #{man} #{maiden} #{cow} #{dog} #{cat} #{rat} #{malt} #{jack}.\n"
+      "#{intro} #{rooster} #{priest} #{man} #{maiden} #{cow} #{dog} #{cat} #{rat} #{malt} #{jack}.\n"
     when num == 11
-      "#{intro} farmer sowing his corn that kept the rooster that crowed in the morn that woke the #{priest} #{man} #{maiden} #{cow} #{dog} #{cat} #{rat} #{malt} #{jack}.\n"
+      "#{intro} farmer sowing his corn that kept the #{rooster} #{priest} #{man} #{maiden} #{cow} #{dog} #{cat} #{rat} #{malt} #{jack}.\n"
     when num == 12
-      "#{intro} horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the #{priest} #{man} #{maiden} #{cow} #{dog} #{cat} #{rat} #{malt} #{jack}.\n"
+      "#{intro} horse and the hound and the horn that belonged to the farmer sowing his corn that kept the #{rooster} #{priest} #{man} #{maiden} #{cow} #{dog} #{cat} #{rat} #{malt} #{jack}.\n"
     end
   end
 
