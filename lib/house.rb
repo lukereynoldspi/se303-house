4da7528c7c6252efcb704b22ed5a65c05e710a70
lukereynoldspi <lukereynoldspi@gmail.com>
2022-05-23 00:04:34 -0700

    created line number class and copied methods

--- lib/house.rb
+++ lib/house.rb
@@ -83,3 +83,88 @@ class House
     1.upto(12).collect { |i| line(i) }.join("\n")
   end
 end
+
+class LineNumber
+  
+  attr_reader :number
+
+  def initialize(number)
+    @number = number
+  end
+
+  def intro
+    "This is the "
+  end
+
+  def jack(num)
+    if num >= 1
+      "house that Jack built"
+    end
+  end
+
+  def malt(num)
+    if num >= 2
+      "malt that lay in the "
+    end
+  end
+
+  def rat(num)
+    if num >= 3
+      "rat that ate the "
+    end
+  end
+
+  def cat(num)
+    if num >= 4
+      "cat that killed the "
+    end
+  end
+
+  def dog(num)
+    if num >= 5
+      "dog that worried the "
+    end
+  end
+
+  def cow(num)
+    if num >= 6
+      "cow with the crumpled horn that tossed the "
+    end
+  end
+
+  def maiden(num)
+    if num >= 7
+      "maiden all forlorn that milked the "
+    end
+  end
+
+  def man(num)
+    if num >= 8
+      "man all tattered and torn that kissed the "
+    end
+  end
+
+  def priest(num)
+    if num >= 9
+      "priest all shaven and shorn that married the "
+    end
+  end
+
+  def rooster(num)
+    if num >= 10
+      "rooster that crowed in the morn that woke the "
+    end
+  end
+
+  def farmer(num)
+    if num >= 11
+      "farmer sowing his corn that kept the "
+    end
+  end
+
+  def horse(num)
+    if num == 12
+      "horse and the hound and the horn that belonged to the "
+    end
+  end
+end
