7da41e4d88ed4f0ae736be16a20ac343b62d6761
lukereynoldspi <lukereynoldspi@gmail.com>
2022-05-22 23:38:59 -0700

    flocked rooster method, added spaces after strings in phrase methods

--- lib/house.rb
+++ lib/house.rb
@@ -20,67 +20,69 @@ class House
     when 9
       "#{intro} #{horse(num)}#{farmer(num)}#{priest} #{man} #{maiden} #{cow} #{dog} #{cat} #{rat} #{malt} #{jack}.\n"
     when 10
-      "#{intro} #{horse(num)}#{farmer(num)}#{rooster} #{priest} #{man} #{maiden} #{cow} #{dog} #{cat} #{rat} #{malt} #{jack}.\n"
+      "#{intro} #{horse(num)}#{farmer(num)}#{rooster(num)} #{priest} #{man} #{maiden} #{cow} #{dog} #{cat} #{rat} #{malt} #{jack}.\n"
     when 11
-      "#{intro} #{horse(num)}#{farmer(num)} #{rooster} #{priest} #{man} #{maiden} #{cow} #{dog} #{cat} #{rat} #{malt} #{jack}.\n"
+      "#{intro} #{horse(num)}#{farmer(num)} #{rooster(num)} #{priest} #{man} #{maiden} #{cow} #{dog} #{cat} #{rat} #{malt} #{jack}.\n"
     when 12
-      "#{intro} #{horse(num)} #{farmer(num)} #{rooster} #{priest} #{man} #{maiden} #{cow} #{dog} #{cat} #{rat} #{malt} #{jack}.\n"
+      "#{intro} #{horse(num)} #{farmer(num)} #{rooster(num)} #{priest} #{man} #{maiden} #{cow} #{dog} #{cat} #{rat} #{malt} #{jack}.\n"
     end
   end
 
   def intro
-    "This is the"
+    "This is the "
   end
 
   def jack
-    "house that Jack built"
+    "house that Jack built "
   end
 
   def malt
-    "malt that lay in the"
+    "malt that lay in the "
   end
 
   def rat
-    "rat that ate the"
+    "rat that ate the "
   end
 
   def cat
-    "cat that killed the"
+    "cat that killed the "
   end
 
   def dog
-    "dog that worried the"
+    "dog that worried the "
   end
 
   def cow
-    "cow with the crumpled horn that tossed the"
+    "cow with the crumpled horn that tossed the "
   end
 
   def maiden
-    "maiden all forlorn that milked the"
+    "maiden all forlorn that milked the "
   end
 
   def man
-    "man all tattered and torn that kissed the"
+    "man all tattered and torn that kissed the "
   end
 
   def priest
-    "priest all shaven and shorn that married the"
+    "priest all shaven and shorn that married the "
   end
 
-  def rooster
-    "rooster that crowed in the morn that woke the"
+  def rooster(num)
+    if num >= 10
+      "rooster that crowed in the morn that woke the "
+    end
   end
 
   def farmer(num)
     if num >= 11
-      "farmer sowing his corn that kept the"
+      "farmer sowing his corn that kept the "
     end
   end
 
   def horse(num)
     if num == 12
-      "horse and the hound and the horn that belonged to the"
+      "horse and the hound and the horn that belonged to the "
     end
   end
 
